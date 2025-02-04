a72fbc06424d50b2820c12b788cb131310209e1e
mattswer31 <songma418@gmail.com>
2025-02-03 19:00:35 -0500

    done

---
+++ codewars.py
@@ -0,0 +1,15 @@
+# Solution: Even or Odd
+def even_or_odd(number):
+  if number % 2 == 0:
+    return "Even"
+  else:
+    return "Odd"
+  
+# Solution: Convert a Number to a String
+def number_to_string(num):
+    return str(num)
+
+# Solution: Remove String Spaces
+def no_space(x):
+    new = x.replace(" ","")
+    return new
